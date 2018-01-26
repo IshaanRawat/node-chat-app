99a7c721d0e02e54c96d1d7cbba1914d14ded985
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-01-26 13:08:25 +0530

    using generateMessage to create message objects

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const socketIO = require("socket.io");
 
+const {generateMessage} = require("./utils/message");
+
 const port = process.env.PORT || 4000;
 const publicPath = path.join(__dirname, "..", "public");
 var app = express();
@@ -20,24 +22,12 @@ io.on("connection", (socket) => {
 
     socket.on("createMessage", (message) => {
         console.log("New Message created:", message);
-        io.emit("newMessage", {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit("newMessage", generateMessage(message.from, message.text));
     });
 
-    socket.emit("newMessage", {
-        from: "Admin",
-        text: "Welcome to the chat app",
-        createdAt: new Date().getTime()
-    });
+    socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app"));
 
-    socket.broadcast.emit("newMessage", {
-        from: "Admin",
-        text: "A new user has joined the chat app",
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit("newMessage", generateMessage("Admin", "New user has joined the chat app"));
 });
 
 server.listen(port, () => {
