26166ba0d49d17889930c276a854f09f02b380a6
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-01-26 17:02:31 +0530

    installed moment js

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,15 +1,19 @@
+var moment = require("moment");
+
 var generateMessage = (from, text) => {
+    var date = moment();
     return {
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: date.valueOf()
     };
 };
 var generateLocationMessage = (from, latitude, longitude) => {
+    var date = moment();
     return {
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt: new Date().getTime()
+        createdAt: date.valueOf()
     };
 };
 
