02de089f54553e5a0797a3c52e45237ab5d2fb11
Ishaan Rawat <ishaan@ishaanrawat.com>
2018-01-26 14:02:25 +0530

    basic send message form added

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 var socket = io();
+
+const messageForm = document.querySelector("#message-form");
         
 socket.on("connect", () => {
     console.log("Connected to the server.");
@@ -10,4 +12,14 @@ socket.on("disconnect", () => {
 
 socket.on("newMessage", (message) => {
     console.log("New Message received!", message);
+});
+
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    socket.emit("createMessage", {
+        from: "User",
+        text: document.querySelector("input[name=message]").value
+    }, (response) => {
+        
+    });
 });
